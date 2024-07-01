4b291314851163419791e17ab98e306de962471d
David Ruiz <jesuruga@gmail.com>
2024-06-30 23:39:03 -0600

    ADD [loginjwt]

--- jwt.js
+++ jwt.js
@@ -1,14 +1,21 @@
+const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
     const bearerHeader = req.headers['authorization'];
     if (typeof bearerHeader !== 'undefined') {
         const bearerToken = bearerHeader.split(" ")[1];
-        req.token = bearerToken;
-        next();
-    }
-    else {
-        res.sendStatus(403);
-    }
 
-}
+        // Verificar el token
+        jwt.verify(bearerToken, process.env.TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(403).send({ message: "Token is not valid" });
+            }
+            req.user = decoded; // Guarda la información decodificada del token en req.user
+            next();
+        });
+    } else {
+        res.status(401).send({ message: "Authorization token is required" });
+    }
+};
+
 module.exports = verifyToken;
